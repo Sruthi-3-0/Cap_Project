9207624c6db248bafaa7a3c491d5f2a7454a92e8
Kanneti Sruthi <148968828+Sruthi-3-0@users.noreply.github.com>
2025-09-12 08:24:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,21 @@ terraform {
       version = "~> 3.0"
     }
   }
+  required_version = ">= 1.0.0"
 }
 
 provider "docker" {}
 
 # Pull the nginx:latest image
 resource "docker_image" "nginx" {
-  name = "nginx:latest"
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
 # Run a container with Nginx
 resource "docker_container" "nginx_container" {
   name  = "nginx_container"
-  image = docker_image.nginx.name
+  image = docker_image.nginx.name   # ✅ THIS IS CORRECT
 
   ports {
     internal = 80
