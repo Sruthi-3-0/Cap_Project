1225ed9e1569456a0a38f6a2f5cbe4d8abe4041f
Kanneti Sruthi <148968828+Sruthi-3-0@users.noreply.github.com>
2025-09-12 08:36:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull the nginx:latest image
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+# Run a container with Nginx
+resource "docker_container" "nginx_container" {
+  name  = "nginx_container"
+  image = docker_image.nginx.name   # ✅ FIXED
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
