6f1c6deba2608d189a2b220cf8855a4092ba4d23
Kanneti Sruthi <148968828+Sruthi-3-0@users.noreply.github.com>
2025-10-01 23:03:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "docker" {}
 # Vault provider reads token from environment variable
 provider "vault" {
   address = "http://127.0.0.1:8200"
-  token   = var.vault_token
+  token   = "root"
 }
 
 # Fetch secret from Vault
