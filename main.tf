817ac5f5d8c740cc75cb406568346de863aa826d
Kanneti Sruthi <148968828+Sruthi-3-0@users.noreply.github.com>
2025-10-01 22:38:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,23 +4,41 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 3.0"
     }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "docker" {}
+provider "vault" {
+  address = "http://127.0.0.1:8200"  
+  token   = "root"                 
+}
+
+# Fetch secret from Vault
+data "vault_generic_secret" "mysecret" {
+  path = "secret/mysecret"
+}
 
 # Pull the nginx:latest image
 resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
 
-# Run a container with Nginx
+# Run a container with Nginx and Vault secrets as env
 resource "docker_container" "nginx_container" {
-  name  = "nginx_container"
+  name  = var.container_name
   image = docker_image.nginx.name
 
   ports {
     internal = 80
-    external = 8081
+    external = var.external_port
   }
+
+  env = [
+    "APP_USERNAME=${data.vault_generic_secret.mysecret.data["username"]}",
+    "APP_PASSWORD=${data.vault_generic_secret.mysecret.data["password"]}"
+  ]
 }
