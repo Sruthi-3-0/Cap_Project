6784eccf419c1fb4d63157e2bf8224aa1601d2de
Sruthi-3-0 <2200032190@kluniversity.in>
2025-10-12 18:54:34 +0530

    modified:   variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
-variable "container_name" {
-  type    = string
-  default = "nginx_demo"
-}
+# variable "container_name" {
+#   type    = string
+#   default = "nginx_demo"
+# }
 
-variable "external_port" {
-  type    = number
-  default = 8081
-}
+# variable "external_port" {
+#   type    = number
+#   default = 8081
+# }
 
-variable "vault_token" {
-  type    = string
-  description = "Vault token is read from environment variable"
-  default = ""  # will use ENV variable, not hardcoded
-}
+# variable "vault_token" {
+#   type    = string
+#   description = "Vault token is read from environment variable"
+#   default = ""  # will use ENV variable, not hardcoded
+# }
