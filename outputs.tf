0f3b6529dc932e95baedd4fd02814534476672e9
Sruthi-3-0 <2200032190@kluniversity.in>
2025-10-12 14:49:59 +0530

    added

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,24 @@
+output "container_id" {
+  description = "ID of the Docker container"
+  value       = docker_container.nginx_container.id
+}
+
 output "container_name" {
-  value = docker_container.nginx_container.name
+  description = "Name of the Docker container"
+  value       = docker_container.nginx_container.name
 }
 
 output "container_ip" {
-  value = docker_container.nginx_container.ip_address
+  description = "IP address of the Docker container"
+  value       = docker_container.nginx_container.network_data[0].ip_address
+}
+
+output "vault_secret_username" {
+  description = "Username stored in Vault secret"
+  value       = vault_kv_secret_v2.mysecret.data["username"]
+}
+
+output "vault_secret_password" {
+  description = "Password stored in Vault secret"
+  value       = vault_kv_secret_v2.mysecret.data["password"]
 }
